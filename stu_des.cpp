17ad065b43005032e5a9c178eca01dca1d13d82c
dikshita kanani <kananidixita05@gmail.com>
2023-10-06 15:24:44 +0530

    student with destructor

---
+++ stu_des.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+    public:
+
+    int dpa,cppm,maths,ic,total,per;
+
+    student()
+    {
+        
+         cout <<"DPA : ";
+         cin>>dpa;
+         cout <<"CPPM : ";
+         cin>>cppm;
+         cout <<"MATHS : ";
+         cin>>maths;
+         cout <<"IC : ";
+         cin>>ic;
+
+    }
+
+            void TOTAL()
+            {
+                   
+                total = dpa+cppm+maths+ic;
+                cout <<"TOTAL IS : "<<total << endl;
+                
+            }
+            ~student()
+            {
+                per = total/4;
+                    cout <<"PER is: "<<per << endl;
+            
+            }
+};
+int main()
+{
+    student s1;
+
+    s1.TOTAL();
+
+}
